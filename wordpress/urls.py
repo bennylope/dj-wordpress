a3dac58c49ca2cc72e98eb91d7222c9cc47014e9
Ben Lopatin <ben@benlopatin.com>
2016-06-20 17:00:32 -0400

    Remove * import and deprecated urlpatterns

--- wordpress/urls.py
+++ wordpress/urls.py
@@ -1,32 +1,33 @@
-from django.conf.urls import *
-from wordpress.views import *
+from django.conf.urls import url
+from wordpress import views
 
-urlpatterns = patterns('wordpress.views',
+
+urlpatterns = [
 
     url(r'^author/(?P<username>[\w-]+)/$',
-        AuthorArchive.as_view(), name='wp_author'),
+        views.AuthorArchive.as_view(), name='wp_author'),
 
     url(r'^category/(?P<term>.+)/$',
-        TaxonomyArchive.as_view(), {'taxonomy': 'category'}, name='wp_taxonomy_category'),
+        views.TaxonomyArchive.as_view(), {'taxonomy': 'category'}, name='wp_taxonomy_category'),
     url(r'^tag/(?P<term>.+)/$',
-        TaxonomyArchive.as_view(), {'taxonomy': 'term'}, name='wp_taxonomy_term'),
+        views.TaxonomyArchive.as_view(), {'taxonomy': 'term'}, name='wp_taxonomy_term'),
     url(r'^taxonomy/(?P<taxonomy>term|category)/(?P<term>.+)/$',
-        TaxonomyArchive.as_view(), name='wp_taxonomy'),
+        views.TaxonomyArchive.as_view(), name='wp_taxonomy'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[^/]+)/(?P<attachment_slug>[^/]+)/$',
-        PostAttachment.as_view(), name='wp_object_attachment'),
+        views.PostAttachment.as_view(), name='wp_object_attachment'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[^/]+)/$',
-        PostDetail.as_view(), name='wp_object_detail'),
+        views.PostDetail.as_view(), name='wp_object_detail'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
-        DayArchive.as_view(), name='wp_archive_day'),
+        views.DayArchive.as_view(), name='wp_archive_day'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',
-        MonthArchive.as_view(), name='wp_archive_month'),
+        views.MonthArchive.as_view(), name='wp_archive_month'),
     url(r'^(?P<year>\d{4})/$',
-        YearArchive.as_view(), name='wp_archive_year'),
+        views.YearArchive.as_view(), name='wp_archive_year'),
 
     url(r'^post/tag/(?P<term_slug>.+)/$',
-        TermArchive.as_view(), name='wp_archive_term'),
+        views.TermArchive.as_view(), name='wp_archive_term'),
     url(r'^$',
-        Archive.as_view(), name='wp_archive_index'),
+        views.Archive.as_view(), name='wp_archive_index'),
 
-)
+]
